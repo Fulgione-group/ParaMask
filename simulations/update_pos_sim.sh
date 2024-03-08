4ba292d9ca1918c18aeddaca7054d5bc18b64ba1
Bastiaan Tjeng <btjeng@nb-s7-022.mpipz.mpg.de>
2024-03-08 14:09:20 +0100

    delete Simulations

--- simulations/update_pos_sim.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk 'BEGIN{FS=OFS="\t"}{if(NR==FNR){if(FNR>1){p[$4]=($1-1)}}else{if(FNR>1){$1=($1+p[$5])}; print $0}}' SVtab_annotated.txt Simulations_SV_SC_tab.txt > Simulations_SV_SC_tab_cpos.txt

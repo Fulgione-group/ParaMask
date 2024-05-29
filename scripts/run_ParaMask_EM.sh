c01ee3b5d4b14bb9314cd811751ba772190f088c
Bastiaan Tjeng <btjeng@nb-s7-022.mpipz.mpg.de>
2024-05-29 15:15:41 +0200

    new scripts

--- scripts/run_ParaMask_EM.sh
+++ scripts/run_ParaMask_EM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Rscript --vanilla $PATH_TO_INSTALLATION_FOLDER/ParaMask/ParaMask_EM_v0.2.5.R\
+Rscript --vanilla $PATH_TO_INSTALLATION_FOLDER/ParaMask/ParaMask_EM_v0.2.7.R\
       	--het $PATH_TO_INSTALLATION_FOLDER/Example_files/Input/Simulations_10PercentDuplications.vcf.het.stat.txt\
         --missingness 0.1\
 	--outdir $PATH_TO_INSTALLATION_FOLDER/Example_files/Output\

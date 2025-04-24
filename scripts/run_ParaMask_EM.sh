6b1027eceb084322c507ed283d533b15ae22dd1c
Bastiaan Tjeng <btjeng@nb-s7-022.mpipz.mpg.de>
2025-04-24 16:43:45 +0200

    change example output and scripts

--- scripts/run_ParaMask_EM.sh
+++ scripts/run_ParaMask_EM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Rscript --vanilla $PATH_TO_INSTALLATION_FOLDER/ParaMask/ParaMask_EM_v0.2.7.1.R\
+Rscript --vanilla $PATH_TO_INSTALLATION_FOLDER/inst/scripts/run_ParaMask_EM.R\
       	--het $PATH_TO_INSTALLATION_FOLDER/Example_files/Input/Simulations_10PercentDuplications.vcf.het.stat.txt\
-        --missingness 0.1\
+        --missingness 0.3\
 	--outdir $PATH_TO_INSTALLATION_FOLDER/Example_files/Output\
-	--ID test
+	--ID testing

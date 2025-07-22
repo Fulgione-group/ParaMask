1bc198ea45732e10be8d27f573f4d805d0163272
Bastiaan Tjeng <btjeng@nb-s7-022.mpipz.mpg.de>
2025-07-22 13:59:24 +0200

    use as.character instead of format()

--- ParaMaskEM/inst/scripts/run_ParaMask_EM.R
+++ ParaMaskEM/inst/scripts/run_ParaMask_EM.R
@@ -159,7 +159,7 @@ if(useRRD){
   })
 }
 
-het$Position <- format(het$Position, scientific = FALSE)
+het$Position <- as.character(het$Position)
 write.table(het, file = paste0(outpath, ID, "_EMresults.het"), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
 het$Position <- as.numeric(het$Position)
 
